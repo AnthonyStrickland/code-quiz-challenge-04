ac3c95d086a2d41f04beb95ce13c99427b35a201
Anthony Strickland <anthonystrickland26@gmail.com>
2023-09-13 19:56:40 -0500

    created pseudo code for index, js, and css.

---
+++ 02-Challenge/js/script.js
@@ -0,0 +1,17 @@
+// get a handle on all html tags,objects,classes,identifiers
+// console.log everything to ensure it works
+// create questions object
+        //create possible answers 
+// save answers to local storage calculate score
+        // high scores page with initials
+// create a timer 5 sec per question
+        // make timer go down by 2 sec when answered incorrectly
+// create a game over tag
+// create bonus question if i have enough time
+// function to start the whole quiz/welcome screen
+// function to change questions when answered
+//  when questions is answered create a correct or incorrect 
+        // the answer on click
+// create a restart or try again button
+
+
